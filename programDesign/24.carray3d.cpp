88b760205d7f818f0ecac960905042dd413ccf7a
Haibara_L <2500013144@stu.pku.edu.cn>
2026-04-03 21:53:26 +0800

    上传STL第一部分笔记和练习

---
+++ programDesign/24.carray3d.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <iomanip> 
+#include <cstring>
+using namespace std;
+template <class T>
+class CArray3D
+{
+// 在此处补充你的代码
+T* ptr;
+int D1,D2,D3;
+struct CArray2D
+{
+    T* ptr1;
+    int D;
+    CArray2D(T* t,int n)
+    {
+        ptr1=t;
+        D=n;
+    }
+    T* operator[](int n)
+    {
+        return ptr1+n*D;
+    }
+    operator T*()
+    {
+        return ptr1;
+    }
+
+};
+public:
+CArray3D(int x,int y,int z)
+{
+    D1=x;
+    D2=y;
+    D3=z;
+    ptr=new T[x*y*z];
+}
+CArray2D operator[](int n)
+{
+    return CArray2D(ptr+n*D2*D3,D3);
+}
+};
+
+CArray3D<int> a(3,4,5);
+CArray3D<double> b(3,2,2);
+void PrintA()
+{
+	for(int i = 0;i < 3; ++i) {
+		cout << "layer " << i << ":" << endl;
+		for(int j = 0; j < 4; ++j) {
+			for(int k = 0; k < 5; ++k) 
+				cout << a[i][j][k] << "," ;
+			cout << endl;
+		}
+	}
+}
+void PrintB()
+{
+	for(int i = 0;i < 3; ++i) {
+		cout << "layer " << i << ":" << endl;
+		for(int j = 0; j < 2; ++j) {
+			for(int k = 0; k < 2; ++k) 
+				cout << b[i][j][k] << "," ;
+			cout << endl;
+		}
+	}
+}
+
+int main()
+{
+
+	int No = 0;
+	for( int i = 0; i < 3; ++ i ) {
+		a[i];
+		for( int j = 0; j < 4; ++j ) {
+			a[j][i];
+			for( int k = 0; k < 5; ++k )
+				a[i][j][k] = No ++;
+			a[j][i][i];	
+		}
+	}
+	PrintA();
+	memset(a[1],-1 ,20*sizeof(int));	
+	memset(a[1],-1 ,20*sizeof(int));
+	PrintA(); 
+	memset(a[1][1],0 ,5*sizeof(int));	
+	PrintA();
+
+	for( int i = 0; i < 3; ++ i )
+		for( int j = 0; j < 2; ++j )
+			for( int k = 0; k < 2; ++k )
+				b[i][j][k] = 10.0/(i+j+k+1);
+	PrintB();
+	int n = a[0][1][2];
+	double f = b[0][1][1];
+	cout << "****" << endl;
+	cout << n << "," << f << endl;
+		
+	return 0;
+}
